1604e226d4114cb3cfe7caeab7e3f9924328cfc7
GortariCL <fernando.gortari@gmail.com>
2022-01-17 23:11:40 -0300

    Sitio Viajes Chile completo con todos los requerimientos

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,10 +13,7 @@ $(document).ready(function () {
 
     var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
     var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-    return new bootstrap.Tooltip(tooltipTriggerEl);
+        return new bootstrap.Tooltip(tooltipTriggerEl);
     });
-
-
-
 
 });
